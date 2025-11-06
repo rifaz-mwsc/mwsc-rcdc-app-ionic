90f402240b607e9695675e28e089db0cba1acc80
Ali Rifaz Waheed <DU12721381@mwsc.com.mv>
2025-11-06 14:29:33 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mwsc-rcdc',
+  webDir: 'www'
+};
+
+export default config;
